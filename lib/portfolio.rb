ddcbaea1f0d2994cffafbee0ed3780dfe63b827b
LewisJPaul <lewis.jp@gmail.com>
2014-04-21 08:04:20 -0400

    ad for On the Scene

--- lib/portfolio.rb
+++ lib/portfolio.rb
@@ -3,7 +3,7 @@ class Portfolio
 	attr_accessor :caption, :link, :photo, :random_element
 
 	def initialize
-		@random_element = rand(3)
+		@random_element = rand(4)
 		@caption = random_caption
 		@link = random_link
 		@photo = random_photo
@@ -14,18 +14,19 @@ class Portfolio
 		"Rock Paper Scissors Lizard Spock, is a twist on the classic playground game made popular by the TV show 
 The Big Bang Theory.  Try your luck.",
 		"Above is a self portrait.  I'm currently in the process of bringing my work to the web.  As I do, this slideshow will be updated.",
-		"A 'Game of Thrones' tribute.  Test your memory of classic lines."
+		"A 'Game of Thrones' tribute.  Test your memory of classic lines.",
+		"Visiting NYC?  Check if a movie was filmed nearby where you're staying."
 		]
 		caption_result[random_element]
 	end
 
 	def random_link
-		link_result = ["'http://r-p-s-l-s.herokuapp.com/'","'/'","'http://game-of-wits.herokuapp.com/'"]
+		link_result = ["'http://r-p-s-l-s.herokuapp.com/'","'/'","'http://game-of-wits.herokuapp.com/'","'http://on-the-scene.herokuapp.com/'"]
 		link_result[random_element]
 	end
 
 	def random_photo
-		photo_result = ["'/img/photo_one.jpg'","'/img/photo_two.png'","'/img/photo_three.jpg'"]
+		photo_result = ["'/img/photo_one.jpg'","'/img/photo_two.png'","'/img/photo_three.jpg'","'/img/photo_four.jpg'"]
 		photo_result[random_element]
 	end
 end
