fb8cec9d95d6ab3911af76661cc3a26799382b28
LewisJPaul <lewis.jp@gmail.com>
2016-01-05 00:31:52 -0500

    less methods more hashes

--- lib/holiday.rb
+++ lib/holiday.rb
@@ -3,26 +3,27 @@ class Holiday
   attr_accessor :title, :css_path, :bodyContent, :todayDate
 
   def initialize
-    @title = title
-    @css_path = cssPath
-    @bodyContent = htmlBodyContent
+    @checkHoliday = checkHoliday(Time.new)
+
+    @title = @checkHoliday["title"]
+    @css_path = @checkHoliday["cssPath"]
+    @bodyContent = @checkHoliday["htmlBodyContent"]
     @todayDate = Time.new
   end
 
-  def htmlBodyContent
-    return "
+  def checkHoliday(whatTime)
+#    if whatTime.month == 12 && whatTime.day = 25
+      return {
+        "htmlBodyContent" => "
       <br><br><br><h1>Merry Christmas!</h1><br><br>
       <img src=\"/img/holiday/santa.png\"><br><br><br><br><br><br><br><br><br>
       <h1><p>Wishing you Peace and Joy this Holiday Season</p></h1>
-    "
-  end
+    ",
+        "title" => "Merry Christmas",
+        "cssPath" => '"/css/holiday/xmas.css"'
+      }
+#    end 
 
-  def title
-    return "Merry Christmas"
-  end
-
-  def cssPath
-    return '"/css/holiday/xmas.css"'
   end
 
 end
